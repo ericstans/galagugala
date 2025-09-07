56f2ab88ea8004685c6c0a1e358ba85e644f0368
Me <ericrvaa@gmail.com>
2025-09-06 21:47:30 -0400

    fix level not affecting enemy behavior correctly

--- src/entities/EnemyManager.js
+++ src/entities/EnemyManager.js
@@ -30,6 +30,7 @@ export class EnemyManager {
   }
 
   createEnemies(level = 1, gameEngine = null) {
+    this.currentLevel = level;
     this.enemies = [];
     
     // Calculate level-based enemy formation size
